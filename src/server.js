b4c4e0069e872fc586e8f9b7842be9869af89a9c
WestleeMa <115126072+WestleeMa@users.noreply.github.com>
2024-04-11 17:33:14 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.get("/", (req, res) => {
   console.log("test");
   res.json({ a: "a" });
